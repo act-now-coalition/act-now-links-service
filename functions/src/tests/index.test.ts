3e520bd6bf329ea40295d08ee4d55cf48aaee00b
Sean McClure <sean.mcclure17@gmail.com>
2022-10-31 19:08:28 -0400

    CR changes

--- functions/src/tests/index.test.ts
+++ functions/src/tests/index.test.ts
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import { API_BASE_URL, createUniqueId } from "../utils";
+import { API_BASE_URL } from "../utils";
 import {
   TEST_PAYLOAD,
   TEST_SHARE_LINK_URL,
@@ -8,18 +8,16 @@ import {
   URL_NOT_FOUND_ERROR,
 } from "./utils";
 
-describe("POST /registerUrl/", () => {
-  test("it returns a 200 and creates a new share link from valid params.", async () => {
-    // the TEST_PAYLOAD params are already registered in setup.ts, so use a different url.
-    const newPayload = { ...TEST_PAYLOAD, url: "https://cdc.gov" };
-    const expectedId = createUniqueId(JSON.stringify(newPayload));
-    const response = await registerUrl(newPayload);
-    const json = await response.json();
-    expect(response.ok).toBe(true);
-    expect(json).toMatchObject({ url: `${API_BASE_URL}/go/${expectedId}` });
-  });
+beforeAll(async () => {
+  if (!process.env.FUNCTIONS_EMULATOR) {
+    throw new Error("Test suite must be run with the Firebase emulator.");
+  }
+  console.log("Registering test url.");
+  return registerUrl(TEST_PAYLOAD); // Register a share link to test against.
+});
 
-  test("it returns the existing share link if one already exists the given params.", async () => {
+describe("POST /registerUrl/", () => {
+  test("returns the existing share link if one already exists with the given params.", async () => {
     const response = await registerUrl(TEST_PAYLOAD); // Re-register the TEST_PAYLOAD share link.
     const json = await response.json();
     expect(response.ok).toBe(true);
@@ -27,7 +25,7 @@ describe("POST /registerUrl/", () => {
   });
 
   test.each([undefined, "ftp://not-a-valid-url"])(
-    "it returns a 400 error for a missing or invalid url.",
+    "returns a 400 error for a missing or invalid url.",
     async (url) => {
       const response = await registerUrl({ ...TEST_PAYLOAD, url: url });
       expect(response.status).toBe(INVALID_URL_ERROR.httpCode);
@@ -36,20 +34,20 @@ describe("POST /registerUrl/", () => {
 });
 
 describe("GET /go/:id", () => {
-  test("it returns a 404 for a non-existing share link.", async () => {
+  test("returns a 404 for a non-existing share link.", async () => {
     const url = `${API_BASE_URL}/go/nonexistent-id`;
     const response = await fetch(url);
     expect(response.status).toBe(URL_NOT_FOUND_ERROR.httpCode);
   });
 
-  test("it returns a 200 for a valid share link.", async () => {
+  test("returns a 200 for a valid share link.", async () => {
     const response = await fetch(TEST_SHARE_LINK_URL);
     expect(response.ok).toBe(true);
   });
 });
 
 describe("GET /shareLinksByUrl", () => {
-  test("it returns expected data for a url with a share link.", async () => {
+  test("returns expected data for a url with a share link.", async () => {
     const url = `${API_BASE_URL}/shareLinksByUrl?url=${TEST_PAYLOAD.url}`;
     const response = await fetch(url);
     const json = await response.json();
@@ -59,7 +57,7 @@ describe("GET /shareLinksByUrl", () => {
     });
   });
 
-  test("it returns an empty object for a url with no share links.", async () => {
+  test("returns an empty object for a url with no share links.", async () => {
     const url = `${API_BASE_URL}/shareLinksByUrl?url=https://wwww.no-link.com`;
     const response = await fetch(url);
     const json = await response.json();
@@ -68,7 +66,7 @@ describe("GET /shareLinksByUrl", () => {
   });
 
   test.each(["", "?url=ftp://not-a-valid-url"])(
-    "it returns a 400 error for a missing or invalid url.",
+    "returns a 400 error for a missing or invalid url.",
     async (urlParam) => {
       const url = `${API_BASE_URL}/shareLinksByUrl${urlParam}`;
       const response = await fetch(url);
@@ -78,8 +76,8 @@ describe("GET /shareLinksByUrl", () => {
 });
 
 describe("GET /screenshot", () => {
-  jest.setTimeout(10000); // Bump timeout up to 10 seconds. Screenshots can take a while.
-  test("it returns a 200 for a valid share-image page.", async () => {
+  jest.setTimeout(15000); // Bump timeout up to 15 seconds. Screenshots can take a while.
+  test("returns a 200 for a valid share-image page.", async () => {
     const imagePage = "https://covidactnow.org/internal/share-image/states/ma";
     const response = await fetch(`${API_BASE_URL}/screenshot?url=${imagePage}`);
     expect(response.ok).toBe(true);
