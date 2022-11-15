6f3034ebe76c0abe629e54d4f75312f2d54437ee
sean mcclure <55333380+smcclure17@users.noreply.github.com>
2022-11-15 16:49:43 -0500

    Set up production Firebase project (#27)
    * Setup production project
    
    * Add fallback firebase project
    
    * Test workflow
    
    * Change service account secret
    
    * decipher environment
    
    * Add final if branch
    
    * Cleanup workflow
    
    * Dont use env, add comments
    
    * Revert changes, keep comments
    
    * replace deprecated set-output
    
    * Just split deploys into two workflows
    
    * Use develop environment for tests
    
    * undo
    
    * Update readme
    
    * replace testing branch with main
    
    * CR changes
    
    * Remove extra lines

--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -3,10 +3,17 @@ import * as firebaseSettings from "../../firebase.json";
 import { firebaseApp } from "./init";
 import { ShareLinkError, ShareLinkErrorCode } from "./error-handling";
 
+const localFunctionsPort = firebaseSettings.emulators.functions.port;
 const isEmulator = process.env.FUNCTIONS_EMULATOR === "true";
+const firebaseConfig = process.env.FIREBASE_CONFIG
+  ? JSON.parse(process.env.FIREBASE_CONFIG)
+  : undefined;
+// When using `firebase emulators:exec` for testing, firebaseConfig
+// is undefined, so use the dev project as a fallback.
+const firebaseProjectId = firebaseConfig?.projectId ?? "act-now-links-dev";
 export const API_BASE_URL = isEmulator
-  ? `http://localhost:${firebaseSettings.emulators.functions.port}/act-now-links-dev/us-central1/api`
-  : "https://us-central1-act-now-links-dev.cloudfunctions.net/api";
+  ? `http://localhost:${localFunctionsPort}/${firebaseProjectId}/us-central1/api`
+  : `https://us-central1-${firebaseProjectId}.cloudfunctions.net/api`;
 export const SHARE_LINK_FIRESTORE_COLLECTION = "share-links";
 
 /** Request body parameters for /registerUrl API calls. */
--- scripts/generate-id-token.ts
+++ scripts/generate-id-token.ts
@@ -4,14 +4,24 @@ import fetch from "node-fetch";
 // TODO: Would prefer to use google login auth to avoid having to enter passwords in the console.
 const email = process.argv[2];
 const password = process.argv[3];
+const projectId = process.argv[4];
 if (!email || !password) {
-  console.log("Usage: yarn generate-id-token.ts <email> <password>");
+  console.error(
+    "Usage: yarn generate-id-token.ts <email> <password> <projectId>"
+  );
+  process.exit(1);
+}
+if (!["develop", "production"].includes(projectId)) {
+  console.error("projectId must be 'develop' or 'production'");
   process.exit(1);
 }
 
 // Firebase Web API key is public/non-confidential.
-// TODO: Hardcoding to act-now-links-dev for now, but this should be configurable in the future.
-const firebaseWebApiKey = "AIzaSyDRW6y_kxN51ZUNJr43y2Mgd6v8BLu-ih4";
+const firebaseWebApiKey =
+  projectId === "develop"
+    ? "AIzaSyDRW6y_kxN51ZUNJr43y2Mgd6v8BLu-ih4"
+    : "AIzaSyBbve3kWHp3b8izyph0puHaPdCLOfkJNww";
+
 fetch(
   `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${firebaseWebApiKey}`,
   {
@@ -34,13 +44,17 @@ fetch(
     const token = data.idToken;
     const expiresIn = data.expiresIn / 3600;
     console.log(`ID token: ${token}`);
-    console.log(`Expires in: ${expiresIn} hours`);
+    console.log(`Expires in: ${expiresIn} hour(s)`);
   })
   .catch((error) => {
-    console.log(
+    const aliasMap = {
+      "develop": "act-now-links-dev",
+      "production": "act-now-links-prod",
+    }
+    console.error(
       "Error while generating ID token. " +
         "Please check that email and password are correct and exist " +
-        "in the act-now-links-dev project auth. \n"
+        `in the ${aliasMap[projectId]} project authentication app. \n`
     );
-    console.log(error);
+    console.error(error);
   });
